3a7765a80475af100c3687cc66731bdee3ba907e
Leah <qptmdi2@naver.com>
2020-01-31 18:05:44 +0900

    문자열에 주어졌을 때, 문자열에서 숫자를 모두 찾아 더한 뒤 해당값을 (숫자,공백을 제외한) 알파벳의 길이로 나눈 값을 정수로 반올림하여 반환하는 메서드, 방법1

---
+++ COPLIT문제/5-3 numberSearch.js
@@ -0,0 +1,37 @@
+// 문자열이 주어졌을 때, 문자열에서 숫자를 모두 찾아 더한 뒤
+// 해당값을 (숫자, 공백을 제외한) 알파벳의 길이의 길이로 나눈 값을
+// 정수로 반올림하여 반환하는 메서드
+// 예 : 문자열 "Hello6 9World 2, Nic8e D7ay!"
+// 6 + 9 + 2 + 8 + 7 = 32
+// 32 / 17 = 1.882
+// 가까운 정수로 반올림 : 2
+
+function numberSearch(str){
+    let numTest = '0123456789'
+
+    let sum = 0;
+    let length = 0;
+    let result ;
+    for(let i=0 ; i<str.length ; i++){
+        if(str.charAt(i) === ' '){
+        }else{
+            let flag = true;
+            for(let j in numTest){
+                if(str.charAt(i) === numTest.charAt(j)){
+                    sum += Number(str.charAt(i));
+                    flag = false;
+                }
+            }
+            if(flag === true){
+                length++;
+            }
+        }
+    }
+
+    result = sum/length;
+    if(result - Math.floor(result) < 0.5){
+        return Math.floor(result);
+    }else{
+        return Math.floor(result)+1;
+    }
+}
