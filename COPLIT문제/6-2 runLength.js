1807e6ba6b27fb7c9e9c3238b0ca5cc20d902315
Leah <qptmdi2@naver.com>
2020-02-01 17:44:58 +0900

    모델솔루션 추가

--- COPLIT문제/6-2 runLength.js
+++ COPLIT문제/6-2 runLength.js
@@ -30,3 +30,28 @@ function runLength(str) {
     result = result.join('');
     return result;
   }
+
+  // 모델솔루션
+  function runLength(str) {
+    var i = 0;
+    var rle = "";
+    while (i < str.length) {
+      var c = str[i];
+      var j = i + 1;
+      var compressed = [1, c];
+  
+      while (j < str.length) {
+        if (str[j] === c) {
+          compressed[0] += 1;
+        } else {
+          break;
+        }
+        j++;
+      }
+  
+      rle += compressed.join('');
+      i = j;
+    }
+  
+    return rle;
+  }
