9d972a87ee13b01bf87498be7ca5f930420acb98
Leah <qptmdi2@naver.com>
2020-02-07 20:16:46 +0900

    Basic 폴더 add

---
+++ COPLIT문제/Basic/convertScoreToGradeWithPlusAndMinus.js
@@ -0,0 +1,25 @@
+// 방법 1
+function convertScoreToGradeWithPlusAndMinus(score){
+    let char = '';
+    if(score>100 || score<0){
+        return "INVALID SCORE";
+    }else if(score>=90){
+        char = 'A';
+    }else if(score>=80){
+        char = 'B';
+    }else if(score>=70){
+        char = 'C';
+    }else if(score>=60){
+        char = 'D';
+    }else{
+        return 'F';
+    }
+
+    if(score === 100 || Number(String(score)[1]) >= 8){
+        char = char + '+';
+    }else if(Number(String(score)[1]) <= 2){
+        char = char + '-';
+    }    
+
+    return char;
+}
