e9184cd3506f2a4dfef54dd4b622fb8c1f1c4d79
Leah <qptmdi2@naver.com>
2020-02-07 20:46:18 +0900

    100점은 A+, 71점은 C- 로 점수에 대응하는 등급을 반환하는 메서드, 3가지 방법

--- COPLIT문제/Basic/convertScoreToGradeWithPlusAndMinus.js
+++ COPLIT문제/Basic/convertScoreToGradeWithPlusAndMinus.js
@@ -1,3 +1,16 @@
+// 점수가 주어졌을 때, 주어진 점수와 대응하는 등급을 문자열로 반환
+// 100 ~ 90 : 'A'
+// 89 ~ 80 : 'B'
+// 79 ~ 70 : 'C'
+// 69 ~ 60 : 'D'
+// 59 ~ 0 : 'F'
+
+// 점수의 1의 자리가 0~2 사이 : '-'
+// 점수의 1의 자리가 8~9 사이 : '+'
+// 100점 : '+'
+// F+ 와 F- 는 존재하지 않음.
+
+
 // 방법 1
 function convertScoreToGradeWithPlusAndMinus(score){
     let char = '';
@@ -22,4 +35,79 @@ function convertScoreToGradeWithPlusAndMinus(score){
     }    
 
     return char;
+}
+
+//my 2번째 풀이
+function convertScoreToGradeWithPlusAndMinus(score){
+    let grade;
+    let one;
+    if( score>100 || score<0 ){
+        return "INVALID SCORE";
+    }else if( score>=90 ){
+        grade = 'A';
+    }else if( score>=80 ){
+        grade = 'B';
+    }else if( score>=70 ){
+        grade = 'C';
+    }else if( score>=60 ){
+        grade = 'D';
+    }else{
+        return 'F';
+    }
+
+    if( score === 100 ){
+        return 'A+';
+    }
+
+    one = (score).toString().slice(1,1) * 1;
+    if( one<=2 ){
+        return grade + '-';
+    }else if( one>=8 ){
+        return grade + '+';
+    }
+}
+
+
+
+// 3번째방법 : 모델 솔루션 (내가 볼 때 제일 좋은 답안)
+function plusOrMinus(score) {
+  if (String(score)[1] <= 2) {
+    return '-';
+  }
+  else if (String(score)[1] >= 8) {
+    return '+';
+  }
+  else {
+    return '';
+  }
+}
+
+function convertScoreToGradeWithPlusAndMinus(score) {
+  let grade;
+  if (score > 100 || score < 0) {
+    return 'INVALID SCORE';
+  }
+  if (score === 100) {
+    return 'A+';
+  }
+  if (score >= 90) {
+    grade = 'A';
+  }
+  else if (score >= 80) {
+    grade = 'B';
+  }
+  else if (score >= 70) {
+    grade = 'C';
+  }
+  else if (score >= 60) {
+    grade = 'D';
+  }
+  else if (score >= 0) {
+    grade = 'F';
+  }
+
+  if (grade !== 'F') {
+    grade = grade + plusOrMinus(score);
+  }
+  return grade;
 }
