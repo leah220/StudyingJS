7fada5e343e98ffc1452914079ff2e3d6d49dd6e
Leah <qptmdi2@naver.com>
2020-02-14 14:24:16 +0900

    문제수정

--- COPLIT문제/Basic/1-6 findShortestOfThreeWords.js
+++ COPLIT문제/Basic/1-6 findShortestOfThreeWords.js
@@ -1,12 +1,12 @@
-// [1.문자열] 06_findShortestOfThreeWords
-// 문자열 3개가 주어졌을 때, 주어진 문자열 중 가장 짧은 문자열 반환.
+// [1.문자열] 06_findShortestOfWords
+// 문자열이 주어졌을 때, 주어진 문자열 중 가장 짧은 문자열 반환.
 
 // 방법 1
 // 전달받은 문자열을 배열로 만든 후,
 // 인덱스 0번 요소를 shortest로 잡고,
 // 나머지 배열 요소와 하나씩 비교하여 문자열에 길이가 짧은 것을 shortest에 할당한다.
 // 반복문이 끝난 뒤에 shortest를 반환한다.
-function findShortestOfThreeWords(...arguments){
+function findShortestOfWords(...arguments){
     let shortest = arguments[0];
     for(let i=1 ; i<arguments.length ; i++){
         if(arguments[i].length < shortest.length){
@@ -19,7 +19,7 @@ function findShortestOfThreeWords(...arguments){
 
 // 방법 2
 // 방법 1과 같은 논리로 reduce를 사용하여 정리한다.
-function findShortestOfThreeWords(...arguments){
+function findShortestOfWords(...arguments){
     return arguments.reduce((shortest, cur)=>{
         return shortest.length <= cur.length ? shortest : cur ; 
     });
