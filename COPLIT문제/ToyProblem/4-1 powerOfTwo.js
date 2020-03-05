c778231f008009d2f9ba662b6616a9fa250388e9
Leah <qptmdi2@naver.com>
2020-03-05 14:41:33 +0900

    parameter가 정수이며, 2의 거듭제곱이면 true를 반환하는 메서드, 2가지 풀이

--- COPLIT문제/ToyProblem/4-1 powerOfTwo.js
+++ COPLIT문제/ToyProblem/4-1 powerOfTwo.js
@@ -1,15 +1,26 @@
 // parameter가 정수이며, 2의 거듭제곱이면 true를 반환한다.
 
+// 방법 1: 내장 메서드 이용
 function powerOfTwo(num){
-    if(Math.floor(num) !== num){
-        return false;
-    }else if(num === 0){
-        return false;
+    if(Math.floor(num) === num 
+        && Math.floor(Math.log2(num)) === Math.log2(num)){
+            return true;
+        }
+    return false;
+}
+
+// 방법 2: 내장 메서드 사용하지 않고 구현
+function powerOfTwo(num){
+    if( num === 1 ){
+        return true;
     }
-    for(let i=3 ; i<Math.floor(num/2); i=i+2){
-        if(num % i === 0){
+    do{
+        num /= 2;
+        if(num===1){
+            return true;
+        }else if(num<1){
             return false;
         }
-    }
-    return true;
+    }while(1);
 }
+
